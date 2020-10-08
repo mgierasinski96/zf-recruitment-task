44b6b18b496237edae85e0d48e35b01d9d10a0fc
mateusz <przykladowy@o3.pl>
2020-10-08 17:26:18 +0200

    initial commit with project setup

---
+++ api/src/main/java/com/gierasinski/zfrecruitmenttask/ZfRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.gierasinski.zfrecruitmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZfRecruitmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZfRecruitmentTaskApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/com/gierasinski/zfrecruitmenttask/ZfRecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gierasinski.zfrecruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZfRecruitmentTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
